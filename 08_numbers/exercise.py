883828044e0e7095198cf57d23d6ce9cc28697d9
Nestor Morales <nestor.morales@hellofresh.nl>
2021-12-09 00:45:02 +0100

    solved 8 a&b

--- 08_numbers/exercise.py
+++ 08_numbers/exercise.py
@@ -4,7 +4,7 @@ Task 2: Infer all numbers with domain and get a 1 to 1 mapping of of the side
 ABCDEFG to the appropriate answer
 """
 
-from typing import List
+from typing import List, Tuple, Dict
 from collections import Counter
 
 # dictionary with numbers
@@ -20,11 +20,18 @@ NUMBERS = {
     8: 'ABCDEFG',
     9: 'ABCDFG'
 }
+
+
 POSSIBLE_CONNECTIONS = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
-# get possible connections into a dictionary
 
-def get_easy_result(output_numbers: List[str]):
+def get_easy_result(output_numbers: List[str]) -> int:
+    """
+    Easy solution
+    1. Count the length of each number (0: ABCEFG)
+    2. Get those ones that have a unique length
+    3. In the output, count the lenghts and increment
+    """
     instances_of_unique_numbers = 0
     list_lengths = [len(value) for value in NUMBERS.values()]
     list_lengths = Counter(list_lengths)
@@ -37,6 +44,15 @@ def get_easy_result(output_numbers: List[str]):
 
 
 def get_mapping_lengths():
+    """
+    Get a dictionary with the key being the length, and value the numbers that have that length
+
+    For instance:
+        - key= 6. Meaning the length of the word
+        - value= [0, 6, 9]. All those numbers have a representation of length 6.
+    This is useful because when we get a random digit of length 6, we can
+    instantly know which numbers could it potentially refer to
+    """
     dictionary_aggregated_lengths = {}
     for key, value in NUMBERS.items():
         if len(value) not in dictionary_aggregated_lengths:
@@ -45,7 +61,10 @@ def get_mapping_lengths():
     return dictionary_aggregated_lengths
 
 
-def load_input(input_path: str):
+def load_input(input_path: str) -> Tuple[List[str], List[str]]:
+    """
+    Loads the input and returns numbers coded (ABDGF) and the output numbers
+    """
     with open(input_path) as f:
         lines = f.readlines()
     numbers_coded = []
@@ -56,13 +75,23 @@ def load_input(input_path: str):
         output_numbers.append(line_numbers_output.split(' '))
     return numbers_coded, output_numbers
 
-def get_dictionay_possible_solutions():
+
+def initialize_domain():
+    """
+    Initialize a domain for each side (A,B,C,D,E,F,G) with lower case (abcdefg)
+    Upper case mean the actual position and lower case means the messed up input
+    """
     dictionary_solutions = {}
     for letter in POSSIBLE_CONNECTIONS:
         dictionary_solutions[letter.upper()] = POSSIBLE_CONNECTIONS.copy()
     return dictionary_solutions
 
-def get_repeated_chars(list_of_words: List[str]):
+
+def get_repeated_chars(list_of_words: List[str]) -> str:
+    """
+    Get which characters a list of words have in common. These characters must
+    be present in all words
+    """
     joined_words = "".join(list_of_words)
     repeated_letters = ''
     for key, value in Counter(joined_words).items():
@@ -71,61 +100,91 @@ def get_repeated_chars(list_of_words: List[str]):
     return repeated_letters
 
 
-def get_complete_decoded_answer(numbers_coded, output_numbers):
+def reduce_domains(row_numbers_coded, mapping_lengths):
+    """
+    Initialize the domain for the problem
+    Then proceed to solve. If there's repeated characters between letters,
+    reduce the domain to those repeated letters (or less)
+    The domain keeps a count of alive answers and is being updated every iteration
+    """
+    # new row
+    domains = initialize_domain()
+    # solve mistery
+    for number_coded in row_numbers_coded:
+        list_number_coded = [char for char in number_coded]
+        numbers_same_length = mapping_lengths[len(number_coded)]
+        numbers_chars = [NUMBERS[digit] for digit in numbers_same_length]
+        repeated_chars = get_repeated_chars(numbers_chars)
+        # reduce domain
+        for char in repeated_chars:
+            alive = set(domains[char])
+            could_be = set(list_number_coded)
+            new_alive = list(alive.intersection(could_be))
+            domains[char] = new_alive
+    return domains
+
+
+def clean_up_domains(domains):
+    """
+    In the final stage, some anwers have been reached. 
+    If there's an answer assigned, let's say A->g, then 'g' needs to be deleted
+    from all the other domains. Do this until all domains have 1 digit.
+    """
+    # initialize solved
+    solved = []
+    for key, value in domains.items():
+        if len(value) == 1:
+            solved.append(key)
+
+    # run until all solved answers have been deleted from other domains
+    while len(solved) > 0:
+        key_to_delete = solved.pop()
+        letter_to_delete = domains[key_to_delete]
+        for key, value in domains.items():
+            if len(value) > 1:
+                # remove from domain
+                new_removed = list(set(value) - set(letter_to_delete))
+                if len(new_removed) == 1:
+                    solved.append(key) # add to solved
+                domains[key] = new_removed
+    return domains
+
+
+def translate_numbers(domains: dict) -> Dict[str, int]:
+    """
+    Given the solved domain, we are going to translate the upper cases into
+    lower cases with the mapping (domain, only 1 to 1 mapping)
+    """
+    numbers_translated = {}
+    for key, value in NUMBERS.items():
+        translated_word = ''
+        for char in value:
+            new_char = domains[char][0]
+            translated_word += new_char
+        numbers_translated[key] = ''.join(sorted(translated_word))
+    numbers_translated = {value: key for key, value in numbers_translated.items()}
+    return numbers_translated
+
+
+def get_complete_decoded_answer(numbers_coded, output_numbers) -> int:
+    """
+    For every row, calculate the sum of the output numbers bu solving the input
+    `numbers_coded`.
+    Return the total sum.
+    """
     mapping_lengths = get_mapping_lengths()
     total_sum = 0
     for row_index, _ in enumerate(numbers_coded):
-        # new row
-        solutions_dict = get_dictionay_possible_solutions()
-        row_numbers_coded = numbers_coded[row_index]
-        row_output_numbers = output_numbers[row_index]
-        # solve mistery
-        for number_coded in row_numbers_coded:
-            list_number_coded = [char for char in number_coded]
-            numbers_same_length = mapping_lengths[len(number_coded)]
-            numbers_chars = [NUMBERS[digit] for digit in numbers_same_length]
-            repeated_chars = get_repeated_chars(numbers_chars)
-            # reduce domain
-            for char in repeated_chars:
-                alive = set(solutions_dict[char])
-                could_be = set(list_number_coded)
-                new_alive = list(alive.intersection(could_be))
-                solutions_dict[char] = new_alive
-        # Clean up if alive
-        solved = []
-        for key, value in solutions_dict.items():
-            if len(value) == 1:
-                solved.append(key)
-        while len(solved) > 0:
-            key_to_delete = solved.pop()
-            letter_to_delete = solutions_dict[key_to_delete]
-            for key, value in solutions_dict.items():
-                if len(value) > 1:
-                    # remove
-                    new_removed = list(set(value) - set(letter_to_delete))
-                    if len(new_removed) == 1:
-                        solved.append(key)
-                    solutions_dict[key] = new_removed
-        numbers_translated = translate_numbers(solutions_dict)
+        domains = reduce_domains(numbers_coded[row_index], mapping_lengths)
+        domains = clean_up_domains(domains)
+        numbers_translated = translate_numbers(domains)
         sum_numbers_string = ''
-        for num in row_output_numbers:
+        for num in output_numbers[row_index]:
             num = ''.join(sorted(num))
             sum_numbers_string += str(numbers_translated[num])
         total_sum += int(sum_numbers_string)
     return total_sum
 
-
-def translate_numbers(solutions_dict: dict):
-    numbers_translated = {}
-    for key, value in NUMBERS.items():
-        translated_word = ''
-        for char in value:
-            new_char = solutions_dict[char][0]
-            translated_word += new_char
-        numbers_translated[key] = ''.join(sorted(translated_word))
-    numbers_translated = {value: key for key, value in numbers_translated.items()}
-    return numbers_translated
-        
 
 def main():
     input_path = '08_numbers/input_hard.txt'
